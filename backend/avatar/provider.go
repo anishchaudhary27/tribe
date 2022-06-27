fbb84efd7cb1fe4138e3cafc65c6d6eb7bb8f3ee
anishchaudhary27 <anishchaudhary2706@gmail.com>
2022-06-27 12:36:42 +0530

    👾added avatar image handler

--- backend/avatar/provider.go
+++ backend/avatar/provider.go
@@ -1,11 +1,37 @@
 package avatar
 
-// func HandleGetAvatar(ctx context.Context, storage *storage.Client) func(*fiber.Ctx) error {
-// 	return func(c *fiber.Ctx) error {
-// 		bucket, err := storage.DefaultBucket()
-// 		if err != nil {
-// 			return fiber.ErrInternalServerError
-// 		}
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"time"
 
-// 	}
-// }
+	"firebase.google.com/go/v4/storage"
+	"github.com/gofiber/fiber/v2"
+)
+
+func HandleGetAvatar(ctx context.Context, storage *storage.Client) func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		bucket, err := storage.DefaultBucket()
+		if err != nil {
+			log.Println(err)
+			return fiber.ErrInternalServerError
+		}
+		ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+		defer cancel()
+		rc, err := bucket.Object("avatars/" + c.Params("id")).NewReader(ctx)
+		if err != nil {
+			log.Printf("Object(%q).NewReader: %v", "avatars/"+c.Params("id"), err)
+			return fiber.ErrNotFound
+		}
+		defer rc.Close()
+		data, err := ioutil.ReadAll(rc)
+		if err != nil {
+			log.Printf("ioutil.ReadAll: %v", err)
+			return fiber.ErrInternalServerError
+		}
+		c.Response().Header.Set("Content-Type", "image")
+		c.Response().Header.Set("Cache-Control", "public, max-age=300, s-maxage=600")
+		return c.Send(data)
+	}
+}
--- backend/server.go
+++ backend/server.go
@@ -7,6 +7,7 @@ import (
 
 	firebase "firebase.google.com/go/v4"
 
+	"github.com/anishchaudhary27/tribe/backend/avatar"
 	"github.com/anishchaudhary27/tribe/backend/config"
 	"github.com/anishchaudhary27/tribe/backend/middleware"
 	"github.com/anishchaudhary27/tribe/backend/user"
@@ -16,9 +17,13 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
+var firebaseConfig = &firebase.Config{
+	StorageBucket: "tribe-a32a2.appspot.com",
+}
+
 func main() {
 	ctx := context.Background()
-	firebaseApp, err := firebase.NewApp(ctx, nil)
+	firebaseApp, err := firebase.NewApp(ctx, firebaseConfig)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
@@ -30,10 +35,10 @@ func main() {
 	if err != nil {
 		log.Fatalf("error initializing firestore: %v\n", err)
 	}
-	// _, err := firebaseApp.Storage(ctx)
-	// if err != nil {
-	// 	log.Fatalf("error initializing firestore: %v\n", err)
-	// }
+	storage, err := firebaseApp.Storage(ctx)
+	if err != nil {
+		log.Fatalf("error initializing firestore: %v\n", err)
+	}
 	appConfig, err := config.GetAppConfig()
 	if err != nil {
 		panic(err)
@@ -46,7 +51,7 @@ func main() {
 	app.Get("/ping", func(c *fiber.Ctx) error {
 		return c.SendString("pong")
 	})
-	// app.Get("/avatar/:id", avatar.HandleGetAvatar(ctx, storage))
+	app.Get("/avatar/:id", avatar.HandleGetAvatar(ctx, storage))
 	api := app.Group("/api")
 	api.Use(middleware.AuthMiddleware(ctx, auth))
 
